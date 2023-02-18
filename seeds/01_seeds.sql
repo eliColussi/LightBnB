845f023e01265bd502747a54e2b55981a9373680
eli <eliitheboss@gmail.com>
2023-02-18 07:26:42 +0000

    INSERT statements to add data to tables

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO users (name, email, password)
+VALUES 
+('Eva Stanley', 'evastan@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Louisa Meyers', 'Lmeyers@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('scooby doo', 'scoob@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+
+
+INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
+VALUES 
+(1, 'round house','description','https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 85234, 6, 6, 7, 'Canada', '7634 morty st', 'victoria', 'british columbia', 79333, true),
+(1, 'square house','description','https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 85234, 1, 6, 3, 'Canada', '111 happy rd', 'victoria', 'british columbia', 83630, true),
+(2, 'triangle house','description','https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 85234, 4, 5, 7, 'Canada', '3467 cya place', 'victoria', 'Alberta', 83897, true);
+
+INSERT INTO reservations (start_date, end_date, property_id, guest_id)
+VALUES 
+('2018-09-11', '2018-09-26', 1, 1),
+('2019-01-04', '2019-02-01', 2, 2),
+('2021-10-01', '2021-10-14', 3, 3);
+
+INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
+VALUES 
+(1, 1, 1, 99, 'messages'),
+(2, 2, 1, 80, 'messages'),
+(3, 3, 2, 95, 'messages');
